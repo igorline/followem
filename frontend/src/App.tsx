dedfbc920224b5a5d9773764fdda90b01090734d
Alex <alex.plutta@googlemail.com>
2023-03-20 22:19:29 -0500

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, goerli, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, gnosis } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { Home } from "./views/home/Home";
+
+const { chains, provider } = configureChains([goerli, polygon, optimism, gnosis], [alchemyProvider({ apiKey: process.env.ALCHEMY_ID! }), publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function App() {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Home />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
---
+++ frontend/src/views/home/Home.tsx
@@ -0,0 +1,8 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <ConnectButton/>
+  </div>
+);
