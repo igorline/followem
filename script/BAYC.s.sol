56a6e66df874c954aab4d89d344fedfd0a6d7d73
Alex <alex.plutta@googlemail.com>
2023-03-21 22:21:30 -0500

    add deployment for BAYC contract

---
+++ script/BAYC.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+import "forge-std/Script.sol";
+import "../src/BoredApeYachtClub.sol";
+
+contract DeployBAYC is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        BoredApeYachtClub nft = new BoredApeYachtClub(
+            "Bored Ape Yacht Club",
+            "BAYC",
+            10000,
+            0
+        );
+
+        vm.stopBroadcast();
+    }
+}
