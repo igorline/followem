3eee5dbc3a4ae1dfd102358c3d505740b46d8fea
Igor Line <igor@line.ee>
2023-03-22 21:23:09 +0100

    feat(contracts): add owner and deadline methods, refactored tests

--- src/crosschainAdvertiser/l2/L2Campaign.sol
+++ src/crosschainAdvertiser/l2/L2Campaign.sol
@@ -3,31 +3,44 @@ pragma solidity ^0.8.15;
 import {getAdHash} from "../AdHash.sol";
 
 contract L2Campaign {
-    //The amount an advertiser will receive for a successful ad
+    // The amount an advertiser will receive for a successful ad
     uint256 public immutable commission;
-    //The contract the campaign is targeting
+    // The contract the campaign is targeting
     address public immutable target;
-    //The address of the forwarder contract
+    // The address of the forwarder contract
     address public immutable l1Forwarder;
-    //The address of the connext contract
+    // The address of the connext contract
     address public immutable connext;
-    //The domain the campaign contract is deployed at
+    // The domain the campaign contract is deployed at
     uint32 public immutable originDomain;
+    // Owner of the campaign
+    address public immutable owner;
+    // Deadline after which the owner can withdraw the funds
+    uint256 public immutable deadline;
 
     mapping(bytes32 => address) claims;
+    uint public claimableRewards;
 
     constructor(
         uint256 _commission,
         address _target,
+        uint256 _deadline,
         address _l1Forwarder,
         address _connext,
         uint32 _originDomain
     ) payable {
+        require(
+            msg.value >= _commission,
+            "Commission cannot be less than total reward added"
+        );
+
         commission = _commission;
         target = _target;
         l1Forwarder = _l1Forwarder;
         connext = _connext;
         originDomain = _originDomain;
+        deadline = _deadline;
+        owner = msg.sender;
     }
 
     modifier onlySource(address _originSender, uint32 _origin) {
@@ -40,10 +53,20 @@ contract L2Campaign {
         _;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyAfter(uint256 _time) {
+        require(block.timestamp >= _time, "Function called too early");
+        _;
+    }
+
     function xReceive(
         bytes32 _transferId,
-        uint256 _amount,
-        address _asset,
+        uint256 _amount, // TODO: ignore
+        address _asset, // TODO: ignore
         address _originSender,
         uint32 _origin,
         bytes memory _callData
@@ -53,30 +76,42 @@ contract L2Campaign {
             (bytes32, address)
         );
         claims[adHash] = advertiser;
+        claimableRewards += commission;
+    }
+
+    function withdraw() external onlyOwner onlyAfter(deadline) {
+        require(
+            address(this).balance > claimableRewards,
+            "Not enough funds to withdraw"
+        );
+        payable(address(msg.sender)).transfer((address(this).balance) - claimableRewards);
     }
 
     function claim(address minter, uint256 tokenId) public payable {
-        //This campaign only supports hardcoded mint but of course campaign can add more selectors on their behalf
-        //In order to claim his reward the advertiser needs to specify which tokenId he wan'ts to claim a reward for.
-        //If this is true the calldata will be exactly the same as the one called be the user
+        // This campaign only supports hardcoded mint but of course campaign can add more selectors on their behalf
+        // In order to claim his reward the advertiser needs to specify which tokenId he wan'ts to claim a reward for.
+        // If this is true the calldata will be exactly the same as the one called be the user
         bytes memory _calldata = abi.encodeWithSignature(
             "mint(address,uint256)",
             minter,
             tokenId
         );
-        //If all params are correcct the hash will equal the one written be xReceive to the claim function. -> claim is authorized
-        bytes32 expectedAddHash = getAdHash(
+        // If all params are correcct the hash will equal the one written be xReceive to the claim function. -> claim is authorized
+        bytes32 expectedAdHash = getAdHash(
             target,
             _calldata,
             address(this),
             msg.sender,
             100
         );
-        require(claims[expectedAddHash] == msg.sender, "unauthorized");
+        require(claims[expectedAdHash] == msg.sender, "unauthorized");
 
-        //A claim can only claime once
-        claims[expectedAddHash] = address(0);
-        //Send the reward to the sender
+        // A claim can only claime once
+        claims[expectedAdHash] = address(0);
+        // Reduce claimable rewards
+        claimableRewards -= commission;
+        // Send the reward to the sender
+        // FIXME: https://consensys.net/diligence/blog/2019/09/stop-using-soliditys-transfer-now/
         payable(address(msg.sender)).transfer(commission);
     }
 }
--- test/l1/Forwarder.t.sol
+++ test/l1/Forwarder.t.sol
@@ -31,22 +31,21 @@ contract ForwarderTest is Test {
 
     uint32 destinationDomain = 100;
     uint256 relayerFee = 1 wei;
-    uint256 comission = 1 wei;
+    uint256 commission = 1 wei;
 
     event AdExecuted(bytes32 indexed adHash, address indexed advertiser);
 
     function setUp() public {
         connextMock = new ConnextMock();
         forwarder = new AdForwarder(IConnext(address(connextMock)));
-        campaign = new L2Campaign(
-            comission,
+        campaign = new L2Campaign{value: 1 ether}(
+            commission,
             target,
+            0,
             address(forwarder),
             address(connextMock),
             destinationDomain
         );
-        //Campaign is funded with 1 ETH
-        vm.deal(address(campaign), 1 ether);
     }
 
     function testForward() public {
@@ -77,28 +76,4 @@ contract ForwarderTest is Test {
         vm.stopPrank();
     }
 
-    function testClaim() public {
-        //The connext bridge is allowed to set the write new entries to the campaign contract
-        vm.prank(address(connextMock));
-
-        //Pretend that the ad was executed
-        campaign.xReceive(
-            0x0,
-            0,
-            address(0),
-            address(forwarder),
-            destinationDomain,
-            abi.encode(
-                0xcfc594dc74253c11f1cb658aefb5183c42bb9d236010c148927607d3006e4f95,
-                advertiser
-            )
-        );
-
-        vm.prank(advertiser);
-        uint balanceBefore = address(advertiser).balance;
-        campaign.claim(minter, 0);
-        uint balanceAfter = address(advertiser).balance;
-        //Advertiser claimed his comission of 1 wei after he prooved that the minter minted token 0 on his behalf
-        assert(balanceAfter == balanceBefore + comission);
-    }
 }
