b65b4f6cbf12ac5f0d4c435678439049cfc7061e
Alex <alex.plutta@googlemail.com>
2023-03-23 22:18:49 -0500

    style mint page

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export const Header = () => {
   return (
-    <Flex justifyContent="end" pt="2" px="4">
-      <ConnectButton />
+    <Flex justifyContent="space-between">
+      <Text>Follow me</Text>
+      <Flex justifyContent="end" pt="2" px="4">
+        <ConnectButton />
+      </Flex>
     </Flex>
   );
 };
