e3d3bb9581499ada2591f6e029d42977eaf4642c
Igor Line <igor@line.ee>
2023-03-24 18:00:31 +0100

    feat(fe): add fetching of the titles for nfts

--- frontend/src/views/allCampaigns/CampaignTile.tsx
+++ frontend/src/views/allCampaigns/CampaignTile.tsx
@@ -1,15 +1,33 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Card, Text, Image } from "@chakra-ui/react";
 import { Result } from "ethers/lib/utils.js";
 
-export const CampaignTile = ({ campaign }: { campaign: Result }) => {
-  const { author, campaign: campaignAddress, commission, target, totalReward } = campaign;
+export const CampaignTile = ({
+  campaign,
+  title,
+}: {
+  campaign: Result;
+  title: String | undefined;
+}) => {
+  const {
+    author,
+    campaign: campaignAddress,
+    commission,
+    target,
+    totalReward,
+  } = campaign;
   return (
-    <Flex>
+    <Card direction={{ base: "column", sm: "row" }}>
+      <Image
+        objectFit="cover"
+        maxW={{ base: "100%", sm: "200px" }}
+        src="https://ipfs.io/ipfs/QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi"
+        alt="Caffe Latte"
+      />
+      <Text>{title || campaignAddress}</Text>
       <Text>{author}</Text>
-      <Text>{campaignAddress}</Text>
       <Text>{commission.toString()}</Text>
       <Text>{target}</Text>
       <Text>{totalReward.toString()}</Text>
-    </Flex>
+    </Card>
   );
 };
