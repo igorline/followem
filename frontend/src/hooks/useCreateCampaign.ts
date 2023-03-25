076b9f7ea7ff853c3b085715513ca11a3734d7fd
Igor Line <igor@line.ee>
2023-03-25 23:43:44 +0100

    feat: emit and store sig for campaign

--- frontend/src/hooks/useCreateCampaign.ts
+++ frontend/src/hooks/useCreateCampaign.ts
@@ -1,20 +1,20 @@
-import { BigNumber, ethers } from "ethers";
-import { useSigner } from "wagmi";
-import { L2CampaingFactory } from "../contracts";
+import {BigNumber, ethers} from "ethers";
+import {useSigner} from "wagmi";
+import {L2CampaingFactory} from "../contracts";
 
 export const useCreateCampaign = () => {
-  const { data: signer } = useSigner();
+  const {data: signer} = useSigner();
 
-  const deployCampaign = async (commision: BigNumber, target: string, deadline: BigNumber, totalReward: BigNumber) => {
+  const deployCampaign = async (commision: BigNumber, target: string, deadline: BigNumber, totalReward: BigNumber, signature: string) => {
     const contract = new ethers.Contract(
       L2CampaingFactory,
-      ["function deployCampaign(uint256 _commission,address _target,uint256 _deadline) external payable"],
+      ["function deployCampaign(uint256 _commission,address _target,uint256 _deadline, string memory _signature) external payable"],
       signer!
     );
 
-    const deployRes = await contract.deployCampaign(commision, target, deadline, { value: totalReward });
+    const deployRes = await contract.deployCampaign(commision, target, deadline, signature, {value: totalReward});
     console.log("deployRes", deployRes);
   };
 
-  return { deployCampaign };
+  return {deployCampaign};
 };
