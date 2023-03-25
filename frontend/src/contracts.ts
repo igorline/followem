d32c5fb9c246c33c568e4797ea7eacdfd6433e30
Alex <alex.plutta@googlemail.com>
2023-03-24 22:42:01 -0500

    style create cmapgin

--- frontend/src/contracts.ts
+++ frontend/src/contracts.ts
@@ -1,6 +1,6 @@
 export const BAYC = "0xb1e5c3f7898e46277eefafa0e8732760e07ffe21";
 export const L1Forwarder = "0x347b77ca840A4aD540371b9F9560f2f394BCcb0F";
-export const L2CampaingFactory = "0x4266fa5cd2b7dd42471ef3b22cd19bfa05f64fb6";
+export const L2CampaingFactory = "0x4CA2e5c75148A54862775fC825c3fFcc4c47d96a";
 
-export const CampaignContract = "0x10Ef4460D7bF737A3560B3FEEdaC345ec1264238";
+export const CampaignContract = "0x9a183487C6650A2c44f279b79bE7B3a27D9F69ac";
 export const Advertiser = "0x09c3d8547020a044c4879cD0546D448D362124Ae";
