56a47f5cc202602327b0b650ab4e2fd6b5dd0dfa
Alex <alex.plutta@googlemail.com>
2023-03-25 21:28:06 -0500

    add txLink to mint page

--- frontend/src/hooks/useAd.ts
+++ frontend/src/hooks/useAd.ts
@@ -1,7 +1,7 @@
-import { calculateRelayerFee, chainIdToDomain, getChainData } from "@connext/nxtp-utils";
 import { BigNumber, ethers } from "ethers";
-import { useSigner } from "wagmi";
+import { useNetwork, useSigner, useSwitchNetwork } from "wagmi";
 import { Advertiser, BAYC, CampaignContract, L1Forwarder } from "../contracts";
+
 //THE BAYC CONTRACT deployed on goerli
 
 //optimism goerli
@@ -11,8 +11,14 @@ const originDomain = "1735353714";
 
 export const useAd = () => {
   const { data: signer } = useSigner();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
 
   const consumeAd = async (value: BigNumber) => {
+    //For the sake of simplicity we only support goerli in this demo frontend. Of course all chains are supported. But implementing every single chain would increase the complexity of this demo application
+    if (chain!.id !== 5) {
+      return await switchNetwork!(5);
+    }
     const iface = new ethers.utils.Interface(["function mintApe(uint numberOfTokens) public payable"]);
     //We mint just one ape at the time
     const calldata = iface.encodeFunctionData("mintApe", [1]);
@@ -28,10 +34,14 @@ export const useAd = () => {
 
     console.log("go relayer fee", relayerFee);
 
-    await contract.consumeAd(BAYC, calldata, CampaignContract, Advertiser, destinationChainId, relayerFee, {
+    const tx = await contract.consumeAd(BAYC, calldata, CampaignContract, Advertiser, destinationChainId, relayerFee, {
       value: value.add(relayerFee),
       gasLimit: 1000000,
     });
+
+    await tx.wait();
+
+    return tx.hash;
   };
   return { consumeAd };
 };
